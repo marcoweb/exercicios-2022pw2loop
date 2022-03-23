1b861f8a8cb066cfd58abfed6e9a85744ae84352
Marco Carvalho <marcocarvalho.web@gmail.com>
2022-03-23 14:02:37 -0300

    Exercício

--- Exercicio1.java
+++ Exercicio1.java
@@ -3,7 +3,7 @@ public class Exercicio1 {
         //Armazena a soma
         int soma = 0;
         // Percorre os números de 1 à 500
-        for(int num = 1;num <= 500;num++) {
+        for(int num = 1;num <= 500;num++) { 
             // Verificar se o número é ímpar
             if((num % 2) != 0) {
                 // Verifica se o número é multiplo de 3
